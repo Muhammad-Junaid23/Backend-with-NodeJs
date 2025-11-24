d3eeeb69551811aadf1e975d97e77c2944b664b4
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-11-25 00:40:55 +0500

    standarized error,response constants

--- project_management_api/src/index.js
+++ project_management_api/src/index.js
@@ -1,18 +1,8 @@
 import { config } from "dotenv";
-import express from "express";
-
+import app from "./app.js";
 config({ path: "./.env" });
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-   res.send("welcome to project management api");
-});
-
-app.get("/unknownX", (req, res) => {
-   res.send("welcome to project management api");
-});
 
 app.listen(port, () => {
    console.log(`App is running at ${port} port`);
