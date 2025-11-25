ca9ae729d4aff45f2e2a0b10a0c3a80ae655fcfb
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-11-26 00:01:35 +0500

    health check route, controller and mongodb connection

--- project_management_api/src/index.js
+++ project_management_api/src/index.js
@@ -1,9 +1,16 @@
 import { config } from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 config({ path: "./.env" });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-   console.log(`App is running at ${port} port`);
-});
+connectDB()
+   .then(() => {
+      app.listen(port, () => {
+         console.log(`App is running at ${port} port`);
+      });
+   })
+   .catch((err) => {
+      console.error("MongoDB connection error", error);
+   });
