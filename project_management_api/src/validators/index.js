257cbaceadc3118a088acce48515f2e6e51d15a5
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-12-20 11:48:59 +0500

    login validation

--- project_management_api/src/validators/index.js
+++ project_management_api/src/validators/index.js
@@ -21,4 +21,11 @@ const userRegisterValidator = () => {
    ];
 };
 
-export { userRegisterValidator };
+const userLoginValidator = () => {
+   return [
+      body("email").optional().isEmail().withMessage("Email is invalid"),
+      body("password").isEmpty().withMessage("password is required"),
+   ];
+};
+
+export { userRegisterValidator, userLoginValidator };
