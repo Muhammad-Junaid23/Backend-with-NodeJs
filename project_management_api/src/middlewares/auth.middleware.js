072f783f170c6b86018344fd6d770d987aeb8b73
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-12-20 12:26:01 +0500

    common auth middleware

---
+++ project_management_api/src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+import { ApiError } from "../utils/api-error";
+import { asyncHandler } from "../utils/async-handler";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+   const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+   if (!token) {
+      throw new ApiError(401, "unauthorized request");
+   }
+
+   try {
+      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(decodedToken?._id).select(
+         "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+      );
+      if (!token) {
+         throw new ApiError(401, "invalid access token");
+      }
+   } catch (error) {
+      throw new ApiError(401, "invalid access token");
+   }
+});
