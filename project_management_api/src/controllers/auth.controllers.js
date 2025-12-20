dac817be4b6df8f4a4a98d0abfbbe1e8535f5151
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-12-20 12:58:37 +0500

    logout user

--- project_management_api/src/controllers/auth.controllers.js
+++ project_management_api/src/controllers/auth.controllers.js
@@ -129,4 +129,28 @@ const login = asyncHandler(async (req, res) => {
       );
 });
 
-export { registerUser, login };
+const logoutUser = asyncHandler(async (req, res) => {
+   await User.findByIdAndUpdate(
+      req.user._id,
+      {
+         $set: {
+            refreshToken: "",
+         },
+      },
+      {
+         new: true,
+      },
+   );
+   const options = {
+      httpOnly: true,
+      secret: true,
+   };
+
+   return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json(new ApiResponse(200, {}, "user logged out"));
+});
+
+export { registerUser, login, logoutUser };
