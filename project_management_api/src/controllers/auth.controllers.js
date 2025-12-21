7bb337d2c8eef41e1961591a29fa017b53d17b45
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-12-21 18:19:16 +0500

    forgot password request controller

--- project_management_api/src/controllers/auth.controllers.js
+++ project_management_api/src/controllers/auth.controllers.js
@@ -288,6 +288,37 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
    }
 });
 
+const forgotPasswordRequest = asyncHandler(async (req, res) => {
+   const { email } = req.body;
+
+   const user = await User.findOne({ email });
+
+   if (!user) {
+      throw new ApiError(404, "User does not exists", []);
+   }
+
+   const { unHashedToken, hashedToken, tokenExpiry } =
+      user.generateTemporaryToken();
+
+   user.forgotPasswordToken = hashedToken;
+   user.forgotPasswordExpiry = tokenExpiry;
+
+   await user.save({ validateBeforeSave: false });
+
+   await sendEmail({
+      email: user?.email,
+      subject: "Password reset request",
+      mailgenContent: forgotPasswordMailgenContent(
+         user.username,
+         `${process.env.FORGOT_PASSWORD_REDIRECT_URL}/${unHashedToken}`,
+      ),
+   });
+
+   return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Password reset email has been sent"));
+});
+
 // const function = asyncHandler(async (req, res) => {})
 
 export {
@@ -298,4 +329,5 @@ export {
    getCurrectUser,
    resendEmailVerification,
    refreshAccessToken,
+   forgotPasswordRequest,
 };
